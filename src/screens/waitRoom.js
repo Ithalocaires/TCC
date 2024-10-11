a1447ed21d59338827d2f919f5b33358a7865354
Ithalo Pimentel de Caires <ithalop.caires@gmail.com>
2024-10-11 19:02:51 -0300

    waitRoom

---
+++ src/screens/waitRoom.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text, TouchableOpacity, Image, StyleSheet } from 'react-native'
+
+const WaitRoom = ({ navigation }) => {
+    return (
+        <Text>TESTE</Text>
+    );
+}
+
+export default WaitRoom;
