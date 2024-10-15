ebf4161b6f2cae126342b3310a54f17b7d65f7bb
Ithalo Pimentel de Caires <ithalop.caires@gmail.com>
2024-10-15 11:03:08 -0300

    Conexão médico-paciente

--- src/screens/chat.js
+++ src/screens/chat.js
@@ -1,114 +1,122 @@
 import { useRoute } from "@react-navigation/native";
-import { StyleSheet } from "react-native";
-
-import { GiftedChat } from 'react-native-gifted-chat'
+import { GiftedChat, Bubble } from 'react-native-gifted-chat';
 import { useCallback, useEffect, useState } from "react";
-
-//importação do firebase e database
-import { collection, addDoc, onSnapshot, query,orderBy } from "firebase/firestore";
+import { collection, addDoc, onSnapshot, query, orderBy } from "firebase/firestore";
 import { database } from "../../config/firebase";
-import { getAnalytics } from "firebase/analytics";
 
 export default function ChatScreen() {
     const [messages, setMessages] = useState([]);
     const route = useRoute();
-    const { sessionId, name, susCard, obs } = route.params;
-    const [id, setId] = useState ('')
-    const [user, setUser] = useState ('')
+    const { sessionId, name, userRole, userId } = route.params; 
 
+    //Pesquisa as mensagens no Firebase
     useEffect(() => {
-      async function getMessages() {
-          const values = query(collection(database, `chatSessions/${sessionId}/messages`), orderBy('createdAt', 'desc'));
-          onSnapshot(values, (snapshot) => {
-            setMessages(
-              snapshot.docs.map(doc => ({
-                  _id: doc.data()._id,
-                  createdAt: doc.data().createdAt.toDate(),
-                  text: doc.data().text,
-                  user: doc.data().user,
-              }))
-          )});
-      }
-      getMessages();
-      }, [sessionId]);
+        const getMessages = async () => {
+            try {
+                const values = query(collection(database, `chatSessions/${sessionId}/messages`), orderBy('createdAt', 'desc'));
+                onSnapshot(values, (snapshot) => {
+                    setMessages(
+                        snapshot.docs.map(doc => ({
+                            _id: doc.id,
+                            createdAt: doc.data().createdAt.toDate(),
+                            text: doc.data().text,
+                            user: doc.data().user,
+                            userRole: doc.data().userRole || 'desconhecido'
+                        }))
+                    );
+                });
+            } catch (error) {
+                console.error("Erro ao buscar mensagens: ", error);
+                //Erro caso o cliente perca conexão com o servidor e não possa buscar mensagens
+            }
+        };
+        getMessages();
+    }, [sessionId]);
 
-  const mensagemEnviada = useCallback((messages = []) => {
-      setMessages(previousMessages => GiftedChat.append(previousMessages, messages));
-      const { _id, createdAt, text, user } = messages[0];
+    const mensagemEnviada = useCallback((messages = []) => {
+        setMessages(previousMessages => GiftedChat.append(previousMessages, messages));
+        const { _id, createdAt, text, user } = messages[0];
 
-      addDoc(collection(database, `chatSessions/${sessionId}/messages`), {
-          _id,
-          createdAt,
-          text,
-          user,
-      });
-  }, [sessionId]);
+        // Verificar se todos os campos necessários estão definidos
+        if (!createdAt) console.error("Campo 'createdAt' indefinido");
+        if (!text) console.error("Campo 'text' indefinido");
+        if (!user) console.error("Campo 'user' indefinido");
+        if (!_id) console.error("Campo '_id' indefinido");
+        if (!user?._id) console.error("Campo 'user._id' indefinido");
+        if (!user?.name) console.error("Campo 'user.name' indefinido");
+        if (!user?.userRole) console.error("Campo 'user.userRole' indefinido");
 
-  
+        if (!createdAt || !text || !user || !_id || !user._id || !user.name || !user.userRole) {
+            console.error("Dados inválidos ao enviar mensagem:", { _id, createdAt, text, user });
+            return;
+        }
+
+        console.log("Enviando mensagem com dados:", { _id, createdAt, text, user, userRole: user.userRole });
+
+        addDoc(collection(database, `chatSessions/${sessionId}/messages`), {
+            _id,
+            createdAt,
+            text,
+            user: {
+                _id: user._id,
+                name: user.name,
+                userRole: user.userRole
+            }
+        }).then(() => {
+            console.log("Mensagem enviada com sucesso!");
+        }).catch((error) => {
+            console.error("Erro ao enviar mensagem: ", error);
+        });
+    }, [sessionId, userRole]);
+
+
+    // Função para estilizar bolha do chat
+    const renderBubble = (props) => {
+        const isCurrentUser = props.currentMessage.user._id === userId;
+        const backgroundColor = isCurrentUser ? '#003770' : '#0071CF';
+        return (
+            <Bubble
+                {...props}
+                wrapperStyle={{
+                    right: {
+                        backgroundColor,
+                        alignSelf: 'flex-end' // Alinha a mensagem enviada à direita
+                    },
+                    left: {
+                        backgroundColor,
+                        alignSelf: 'flex-start' // Alinha a mensagem recebida à esquerda
+                    }
+                }}
+                textStyle={{
+                    right: {
+                        color: '#fff'
+                    },
+                    left: {
+                        color: '#fff'
+                    }
+                }}
+            />
+        );
+    };
 
     return (
         <GiftedChat
-          messages={messages}
-          onSend={msg => mensagemEnviada(msg)}
-          user={{
-                _id: name,
+            messages={messages}
+            onSend={msg => mensagemEnviada(msg)}
+            user={{
+                _id: userId || 'anon',
+                name: name || 'Anônimo',
+                userRole: userRole || 'desconhecido'
             }}
+            renderBubble={renderBubble}
+            renderAvatar={null}
+            textInputStyle={{
+                color: '#000',
+                backgroundColor: '#FFF'
+            }}
+            isCustomViewBottom={false}
+            keyboardShouldPersistTaps='handled' // Garante que o teclado não feche inesperadamente
+            alwaysShowSend 
         />
-    )
-};
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 16,
-  },
-  userMessage: {
-    backgroundColor: '#003770',
-    padding: 10,
-    borderRadius: 8,
-    marginBottom: 8,
-    borderColor: 'black',
-    alignSelf: 'flex-end',
-    marginStart: 100,
-  },
-  docMessage: {
-    backgroundColor: '#0071CF',
-    padding: 10,
-    borderRadius: 8,
-    alignSelf: 'flex-start',
-    marginBottom: 8,
-    marginEnd: 100,
-  },
-  messageText: {
-    fontSize: 16,
-    color: 'white',
-  },
-  inputContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    borderTopWidth: 1,
-    borderColor: '#ccc',
-    paddingTop: 8,
-  },
-  input: {
-    flex: 1,
-    borderWidth: 1,
-    borderColor: '#ccc',
-    borderRadius: 4,
-    padding: 8,
-    marginRight: 8,
-    color:'black',
-  },
-  sendButton: {
-    padding: 12,
-    borderRadius: 8,
-    alignItems: 'center',
-    borderRadius: 20,
-    backgroundColor: '#0071CF',
-    color:'white',
-  },
-  timestamp: {
-    fontSize: 8,
-    color: 'white',
-  },
-});
+    );
+}
--- src/screens/waitRoom.js
+++ src/screens/waitRoom.js
@@ -1,10 +1,111 @@
-import React from 'react';
-import { View, Text, TouchableOpacity, Image, StyleSheet } from 'react-native'
+import React, { useEffect, useState } from 'react';
+import { FlatList, TouchableOpacity, Text, View, ActivityIndicator, StyleSheet } from 'react-native';
+import { collection, getDocs, onSnapshot, doc, updateDoc, setDoc, query, orderBy } from '@firebase/firestore';
+import { database } from "../../config/firebase";
 
-const WaitRoom = ({ navigation }) => {
-    return (
-        <Text>TESTE</Text>
-    );
-}
+const WaitRoom = ({ navigation, route }) => {
+    const { userRole, userId } = route.params; // Adicione userId às props de navegação
+    const [pacientes, setPacientes] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    //Busca os pacientes em espera no Firabase
+    useEffect(() => {
+        const fetchPacientes = async () => {
+            try {
+                const q = query(collection(database, 'waitRoom'), orderBy('createdAt', 'desc')); // Ordena do mais recente para o mais antigo como uma pilha
+                const pacientesCollection = await getDocs(q);
+                const pacientesList = pacientesCollection.docs.map(doc => ({ 
+                    ...doc.data(), 
+                    id: doc.id 
+                }));
+                setPacientes(pacientesList);
+                setLoading(false);
+            } catch (error) {
+                console.error('Erro ao buscar pacientes:', error);
+                setLoading(false);
+            }
+        };
+
+        if (userRole === 'medico') {
+            fetchPacientes();
+        }
+    }, [userRole]);
+
+    useEffect(() => {
+        if (userRole === 'paciente') {
+            const unsubscribe = onSnapshot(doc(database, 'waitRoom', userId), (doc) => {
+                const data = doc.data();
+                if (data && data.chatActive) {
+                    navigation.navigate('Chat', { sessionId: data.sessionId, name: data.name, susCard: data.susCard, obs: data.obs, userRole: 'paciente', userId: userId }); // Passa userId
+                }
+            });
+            return () => unsubscribe();
+        }
+    }, [userRole, userId]);
+
+    const handleSelectPaciente = async (paciente) => {
+        await updateDoc(doc(database, 'waitRoom', paciente.id), {
+            chatActive: true,
+            sessionId: paciente.id
+        });
+        navigation.navigate('Chat', { sessionId: paciente.id, name: paciente.name, userRole: 'medico', userId: 'medico' }); // Passa userId como 'medico'
+    };
+
+    // Tela de loading enquanto o Paciente aguarda um médico atende-lo
+    if (loading) {
+        return (
+            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+                <ActivityIndicator size="large" color="#0000ff" />
+                {userRole === 'paciente' && <Text>Aguardando atendimento...</Text>}
+            </View>
+        );
+    }
+
+    // A mensagem irá aparecer caso não tenha nenhum paciente na fila
+    if (userRole === 'medico') {
+        if (pacientes.length === 0) {
+            return <Text>Nenhum paciente aguardando atendimento.</Text>;
+        }
+        return (
+            <View style={styles.container}>
+                <FlatList
+                    data={pacientes}
+                    keyExtractor={item => item.id}
+                    renderItem={({ item }) => (
+                        <TouchableOpacity style={styles.pacienteContainer} onPress={() => handleSelectPaciente(item)}>
+                            <Text style={styles.pacienteNome}>Nome: {item.name}</Text>
+                            <Text style={styles.pacienteSusCard}>SUS Card: {item.susCard}</Text>
+                        </TouchableOpacity>
+                    )}
+                />
+            </View>
+        );
+    }
+
+    return null;
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: 10,
+    },
+    pacienteContainer: {
+        padding: 15,
+        marginVertical: 10,
+        backgroundColor: '#f9f9f9',
+        borderRadius: 8,
+        borderColor: '#ddd',
+        borderWidth: 1,
+    },
+    pacienteNome: {
+        fontSize: 16,
+        fontWeight: 'bold',
+    },
+    pacienteSusCard: {
+        fontSize: 14,
+        color: '#555',
+    }
+});
 
 export default WaitRoom;
